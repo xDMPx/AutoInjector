d0981548c43b32e03cb9ce4da3bb9e3239585c3b
xDMPx <57721731+xDMPx@users.noreply.github.com>
2025-10-16 19:34:27 +0200

    Inject script only ones per webpage

--- src/background.ts
+++ src/background.ts
@@ -6,14 +6,16 @@ chrome.tabs.onActivated.addListener(async (activeInfo: chrome.tabs.OnActivatedIn
         args: [code],
         //injectImmediately: true,
         world: "MAIN",
-        func: (c) => {
-            console.log(`AutoInjector; Script Injection => ${c}`);
+        func: (code) => {
+            const auto_injector_script = document.getElementById("autoinjector-script");
+            if (auto_injector_script !== null) return;
+            console.log(`AutoInjector; Script Injection => ${code}`);
             const script = document.createElement("script");
             script.type = "text/javascript";
-            script.text = c;
+            script.id = "autoinjector-script";
+            script.text = code;
             document.body.appendChild(script);
             console.log(script);
-
         }
     });
-})
+});
