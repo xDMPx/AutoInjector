bb6c7e7ef79e854951b45eb12bc36bfd95010ca3
xDMPx <57721731+xDMPx@users.noreply.github.com>
2025-10-17 19:24:40 +0200

    Trigger script injection on webpage reload and navigation

--- src/background.ts
+++ src/background.ts
@@ -7,33 +7,51 @@ chrome.tabs.onActivated.addListener(async (activeInfo: chrome.tabs.OnActivatedIn
             args: [code, djb2_hash(code)],
             //injectImmediately: true,
             world: "MAIN",
-            func: (code: string, hash: number) => {
-                const auto_injector_script = document.getElementById(`autoinjector-script-${hash}`);
-                if (auto_injector_script !== null) return;
-
-                console.log(`AutoInjector; Script Injection => ${code}`);
-
-                // Crate Trusted Types policy to allow injection on website with Trusted Types requirements 
-                if (window.trustedTypes !== undefined) {
-                    if (window.trustedTypes.defaultPolicy === null) {
-                        window.trustedTypes?.createPolicy('default', {
-                            createScript: (input: string) => {
-                                return input;
-                            }
-                        });
-                    }
-                }
-
-                const script = document.createElement("script");
-                script.type = "text/javascript";
-                script.id = `autoinjector-script-${hash}`;
-                script.text = code;
-                document.body.appendChild(script);
-                console.log(script);
-            }
+            func: inject_script,
         });
     }
 });
+
+
+chrome.tabs.onUpdated.addListener(async (tabId: number, updateinfo: chrome.tabs.OnUpdatedInfo) => {
+    if (updateinfo.status === chrome.tabs.TabStatus.COMPLETE) {
+        for (const code of scripts) {
+            await chrome.scripting.executeScript({
+                target: { tabId: tabId },
+                args: [code, djb2_hash(code)],
+                //injectImmediately: true,
+                world: "MAIN",
+                func: inject_script,
+            });
+        }
+    }
+});
+
+function inject_script(code: string, hash: number) {
+    const auto_injector_script = document.getElementById(`autoinjector-script-${hash}`);
+    if (auto_injector_script !== null) return;
+
+    console.log(`AutoInjector; Script Injection => ${code}`);
+
+    // Crate Trusted Types policy to allow injection on website with Trusted Types requirements 
+    if (window.trustedTypes !== undefined) {
+        if (window.trustedTypes.defaultPolicy === null) {
+            window.trustedTypes?.createPolicy('default', {
+                createScript: (input: string) => {
+                    return input;
+                }
+            });
+        }
+    }
+
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.id = `autoinjector-script-${hash}`;
+    script.text = code;
+    document.body.appendChild(script);
+    console.log(script);
+
+}
 
 // http://www.cse.yorku.ca/~oz/hash.html
 function djb2_hash(str: string): number {
