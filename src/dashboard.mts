xDMPx/AutoInjector

commit eb74edcf935851f642dd5b79c4e946ba9d312b6b

Require non‑empty name (min 3 chars) and JavaScript content on script submission

src/dashboard.mts
@@ -26,8 +26,8 @@ async function main() {
         }
     }
 
-    const submit_script = document.getElementById("submit-script")!;
-    submit_script.onclick = saveScript;
+    const submit_script = document.getElementById("submit-script-form")!;
+    submit_script.onsubmit = (e) => { saveScript(e); };
 
     const user_script_text = document.getElementById("user-script") as HTMLTextAreaElement;
     user_script_text.oninput = () => { autoResizeTextArea() };
@@ -200,7 +200,8 @@ function getLineIndent(line: string): string {
     return line.slice(0, i);
 }
 
-async function saveScript() {
+async function saveScript(e: SubmitEvent) {
+    e.preventDefault();
     const user_script_text = document.getElementById("user-script") as HTMLTextAreaElement;
     const user_script_name = document.getElementById("user-script-name") as HTMLInputElement;
     await saveAutoInjectorScript(user_script_name.value, user_script_text.value);

src/dashboard.html
@@ -14,12 +14,13 @@
 
 <body>
     <div class="w-3/4 mx-auto">
-        <div class="grid p-4 gap-2">
-            <input id="user-script-name" type="text" placeholder="Script name"
-                class="input w-auto outline outline-offset-[unset]" />
-            <textarea class="outline p-4" id="user-script" spellcheck="false" placeholder="JavaScript"></textarea>
+        <form id="submit-script-form" class="grid p-4 gap-2">
+            <input id="user-script-name" type="text" required placeholder="Script name"
+                class="input validator w-auto outline outline-offset-[unset]" minlength="3" />
+            <textarea required class="outline p-4" id="user-script" spellcheck="false"
+                placeholder="JavaScript"></textarea>
             <button class="btn btn-primary" id="submit-script">Submit</button>
-        </div>
+        </form>
         <div id="script-div"></div>
     </div>
     <div class="fab">

commit c4bf77c294a1897656260c3a8ec405809af33651

Fix bug introduced in eb74edcf93 causing edit mode to create duplicate script. Form on submit event was triggering script saving.

src/dashboard.mts
@@ -26,8 +26,8 @@ async function main() {
         }
     }
 
-    const submit_script = document.getElementById("submit-script-form")!;
-    submit_script.onsubmit = (e) => { saveScript(e); };
+    const submit_script_form = document.getElementById("submit-script-form") as HTMLFormElement;
+    submit_script_form.onsubmit = (e) => { saveScript(e); };
 
     const user_script_text = document.getElementById("user-script") as HTMLTextAreaElement;
     user_script_text.oninput = () => { autoResizeTextArea() };
@@ -115,10 +115,11 @@ function createScriptCollapse(name: string, code: string): HTMLDivElement {
 function editScriptMode(i: number, name: string, script: string) {
     const user_script_text = document.getElementById("user-script") as HTMLTextAreaElement;
     const user_script_name = document.getElementById("user-script-name") as HTMLTextAreaElement;
-    const submit_script = document.getElementById("submit-script")!;
+    const submit_script_button = document.getElementById("submit-script") as HTMLButtonElement;
+    const submit_script_form = document.getElementById("submit-script-form") as HTMLFormElement;
     user_script_text.style.height = 'auto';
-    submit_script.textContent = "Save";
-    submit_script.onclick = () => { editScript(i) };
+    submit_script_button.textContent = "Save";
+    submit_script_form.onsubmit = (e) => { editScript(e, i) };
     user_script_text.value = script;
     user_script_text.style.height = `${user_script_text.scrollHeight}px`;
     user_script_name.value = name;
@@ -222,7 +223,8 @@ async function deleteScript(i: number) {
     };
 }
 
-async function editScript(i: number) {
+async function editScript(e: SubmitEvent, i: number) {
+    e.preventDefault();
     const user_script_text = document.getElementById("user-script") as HTMLTextAreaElement;
     const user_script_name = document.getElementById("user-script-name") as HTMLTextAreaElement;
     await editAutoInjectorScript(i, user_script_name.value, user_script_text.value);